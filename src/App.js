439619010abaaaeeecef049e5fd5bf80fb4f315f
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-07-15 21:04:07 +0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Cards,Chart,CountryPicker} from './components';
+import styles from './App.module.css';
+function App() {
+  return (
+    <div className={styles.container}>
+      <Cards/>
+      <CountryPicker/>
+      <Chart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CountryPicker() {
+    return (
+        <h1>Country picker</h1>
+    )
+}
