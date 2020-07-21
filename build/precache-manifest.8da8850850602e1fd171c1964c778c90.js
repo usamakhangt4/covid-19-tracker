c316e9ade2d5c18abaca3d86299c56293585cec4
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-07-21 12:06:33 +0500

    final

---
+++ build/precache-manifest.8da8850850602e1fd171c1964c778c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bec4e5a5fb596789d4fe7c5d3d6e148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75712e6013c27d782b83",
+    "url": "/static/css/main.0c8101b0.chunk.css"
+  },
+  {
+    "revision": "d2d0570fe275697f3489",
+    "url": "/static/js/2.052dd467.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.052dd467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75712e6013c27d782b83",
+    "url": "/static/js/main.beb16d56.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid-19.d7265326.png"
+  }
+]);
